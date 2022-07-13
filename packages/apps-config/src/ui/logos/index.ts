e32c1696a78734f441cba16b973982c84ab5f76d
frank <450595468@qq.com>
2022-07-13 17:24:16 +0800

    Add Live Network Odyssey (#7866)
    * update json
    
    * Update for Odyssey
    
    * remove todo.
    
    * update Odyssey
    
    * Fix odyssey - named logo
    
    * update sort
    
    * update sort
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * support ares-chain v1.1.0
    
    * update ares pic
    
    * support ares-chain v1.1.0
    
    * update gladios png
    
    * support ares-chain v1.1.0
    
    * update ares-gladios.ts
    
    * update ares-gladios.ts
    
    * fix pic not find
    
    * sort order
    
    * remove repetitive
    
    * update ares-gladios.ts
    
    * Update type for runtime 106
    
    * Add parachain-mars type json.
    
    * Add parachain-odyssey logos/index.ts
    
    * Update AccountParticipateEstimates, bsc_address, estimates_id
    
    * update index.ts
    
    * Update ares odyssey.
    
    * update order
    
    * update order
    
    Co-authored-by: linhai <kami@cancanyou.com>
    Co-authored-by: zzm <zzmwork8@gmail.com>

--- packages/apps-config/src/ui/logos/index.ts
+++ packages/apps-config/src/ui/logos/index.ts
@@ -87,6 +87,7 @@ import nodeArctic from './nodes/arctic.png';
 import nodeAres from './nodes/ares.png';
 import nodeAresGladios from './nodes/ares-gladios.svg';
 import nodeAresMars from './nodes/ares-mars.png';
+import nodeAresOdyssey from './nodes/ares-odyssey.svg';
 import nodeAstar from './nodes/astar.png';
 import nodeAutomata from './nodes/automata.png';
 import nodeBajun from './nodes/bajun.png';
@@ -223,6 +224,7 @@ export const chainLogos = Object.entries({
   'Apron PC1': nodeApron,
   Arctic: nodeArctic,
   'Ares Gladios': nodeAresGladios,
+  'Ares Odyssey': nodeAresOdyssey,
   'Ares PC1': nodeAres,
   Astar: chainAstar,
   Automata: nodeAutomata,
@@ -340,7 +342,7 @@ export const chainLogos = Object.entries({
   NFTMart: nodeNFTMart,
   'NFTMart Staging': nodeNFTMart,
   'NFTMart Testnet': nodeNFTMart,
-  Odyssey: nodeAresGladios,
+  Odyssey: nodeAresOdyssey,
   OmniBTC: chainOmniBTC,
   'OPAL by UNIQUE': chainOpal,
   'OriginTrail Parachain': chainOriginTrail,
@@ -434,6 +436,7 @@ export const nodeLogos = Object.entries({
   Arctic: nodeArctic,
   'Ares Gladios': nodeAresGladios,
   'Ares Node': nodeAres,
+  'Ares Odyssey': nodeAresOdyssey,
   'Ares Parachain Collator': nodeAres,
   Astar: nodeAstar,
   'Automata ContextFree Node': nodeAutomata,
@@ -639,6 +642,7 @@ export const namedLogos: Record<string, unknown> = {
   amplitude: chainAmplitude,
   arctic: nodeArctic,
   'Ares Gladios': nodeAresGladios,
+  'Ares Odyssey': nodeAresOdyssey,
   astar: chainAstar,
   automata: nodeAutomata,
   'automata-contextfree': nodeAutomata,
@@ -737,7 +741,7 @@ export const namedLogos: Record<string, unknown> = {
   myriad: chainMyriad,
   nftmart: nodeNFTMart,
   nodle: nodeNodle,
-  odyssey: nodeAresGladios,
+  odyssey: nodeAresOdyssey,
   omnibtc: nodeOmniBTC,
   opal: nodeUniqueWestend,
   opportunity: nodeOpportunity,
