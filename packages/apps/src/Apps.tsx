6618cc526aa245239d2946f860e228139f7fd1dc
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-05-30 18:19:07 +0200

    no unsigned validator error

--- packages/apps/src/Apps.tsx
+++ packages/apps/src/Apps.tsx
@@ -3,13 +3,15 @@
 
 import type { BareProps as Props } from '@polkadot/react-components/types';
 
-import React, { useMemo } from 'react';
+import * as snap from 'azero-snap-adapter';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import AccountSidebar from '@polkadot/app-accounts/Sidebar';
 import { styled } from '@polkadot/react-components/styled';
 import GlobalStyle from '@polkadot/react-components/styles';
 import { useApi, useTheme } from '@polkadot/react-hooks';
 import Signer from '@polkadot/react-signer';
+import { keyring } from '@polkadot/ui-keyring';
 
 import Content from './Content/index.js';
 import Menu from './Menu/index.js';
@@ -23,6 +25,48 @@ function Apps ({ className = '' }: Props): React.ReactElement<Props> {
   const { themeClassName } = useTheme();
   const { apiEndpoint, isDevelopment } = useApi();
 
+  const [snapConnected, setSnapConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    const connectSnap = async () => {
+      const isEnabled = (await snap.getAccounts()).length > 0;
+
+      setLoading(isEnabled);
+      setSnapConnected(isEnabled);
+    };
+
+    connectSnap().catch(console.error);
+  }, []);
+
+  const doConnectSnap = async () => {
+    setLoading(true);
+    setSnapConnected(false);
+
+    try {
+      await snap.connect();
+      setSnapConnected(true);
+      const accounts = await snap.getAccounts();
+
+      if (accounts.length < 1) {
+        if (seed === '') {
+          const account = await snap.generateNewAccount();
+
+          keyring.addExternal(account.address);
+        } else {
+          const account = await snap.getAccountFromSeed(seed);
+
+          keyring.addExternal(account.address);
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    setLoading(false);
+  };
+
   const uiHighlight = useMemo(
     () => isDevelopment
       ? undefined
@@ -32,19 +76,60 @@ function Apps ({ className = '' }: Props): React.ReactElement<Props> {
 
   return (
     <>
-      <GlobalStyle uiHighlight={uiHighlight} />
-      <StyledDiv className={`${className} apps--Wrapper ${themeClassName}`}>
-        <Menu />
-        <AccountSidebar>
-          <Signer>
-            <Content />
-          </Signer>
-          <ConnectingOverlay />
-          <DotAppsOverlay />
-          <div id={PORTAL_ID} />
-        </AccountSidebar>
-      </StyledDiv>
-      <WarmUp />
+      {!snapConnected && (
+        <>
+          <h1>Connect and install snap</h1>
+          <p>
+            New Aleph Zero account will be automatically created from your MetaMask private key.
+          </p>
+          <p>
+            Please take a not of snap permission, that you will be asked for.
+          </p>
+          <p>
+            Note: We recommend using a throw-away MetaMask account.
+          </p>
+          <p>
+            This demo uses MetaMask flask (canary release). In order to use it, please follow installation instructions
+            in readme: https://github.com/piotr-roslaniec/ethwarsaw-2022/tree/docs#installing-metamask-flask.
+          </p>
+          {loading && <p>Loading...</p>}
+          {!loading &&
+            <div>
+              <div>
+                <input
+                  height={'40px'}
+                  onChange={(e) => setSeed(e.target.value)}
+                  placeholder={'seed in hex (optional)'}
+                ></input>
+              </div>
+              <div>
+                <button
+                  disabled={snapConnected}
+                  onClick={doConnectSnap}
+                >
+                  Create account
+                </button>
+              </div>
+            </div>}
+        </>
+      )}
+      {snapConnected && (
+        <>
+          <GlobalStyle uiHighlight={uiHighlight} />
+          <StyledDiv className={`${className} apps--Wrapper ${themeClassName}`}>
+            <Menu />
+            <AccountSidebar>
+              <Signer>
+                <Content />
+              </Signer>
+              <ConnectingOverlay />
+              <DotAppsOverlay />
+              <div id={PORTAL_ID} />
+            </AccountSidebar>
+          </StyledDiv>
+          <WarmUp />
+        </>
+      )}
     </>
   );
 }
--- packages/react-signer/src/signers/index.ts
+++ packages/react-signer/src/signers/index.ts
@@ -5,3 +5,4 @@ export { AccountSigner } from './AccountSigner.js';
 export { ApiSigner } from './ApiSigner.js';
 export { LedgerSigner } from './LedgerSigner.js';
 export { QrSigner } from './QrSigner.js';
+export { MetaMaskSnapSigner } from './MetaMaskSnapSigner.js';
