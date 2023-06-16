5878b68f4de157af1af391b5e86e7692d97d4e4d
Wojciech Basiura <basiura.wojciech@gmail.com>
2023-06-16 13:54:22 +0200

    A0-2404: Add lenient threshold support

---
+++ packages/react-hooks/src/useLenientThresholdPercentage.ts
@@ -0,0 +1,34 @@
+import type { Perquintill } from '@polkadot/types/interfaces/runtime';
+
+import getCommitteeManagement from '@polkadot/react-api/getCommitteeManagement';
+import { BN, BN_QUINTILL } from '@polkadot/util';
+
+import { createNamedHook } from './createNamedHook.js';
+import { useApi } from './useApi.js';
+import { useCall } from './useCall.js';
+
+const BACKWARD_COMPATIBLE_LENIENT_THRESHOLD = 90;
+
+// preserved percentage precision is four digits - e.g. 57.75%
+const PERCENTAGE_MUL = new BN(10_000);
+
+function useLenientThresholdPercentageImpl (): number | undefined {
+  const { api } = useApi();
+
+  const lenientThresholdFn = getCommitteeManagement(api).query.lenientThreshold;
+  const isLenientThresholdDefined = !!lenientThresholdFn;
+
+  const lenientThresholdPerquintill = useCall<Perquintill>(lenientThresholdFn);
+
+  if (!isLenientThresholdDefined) {
+    return BACKWARD_COMPATIBLE_LENIENT_THRESHOLD;
+  }
+
+  if (lenientThresholdPerquintill === undefined) {
+    return undefined;
+  }
+
+  return lenientThresholdPerquintill.mul(PERCENTAGE_MUL).div(BN_QUINTILL).toNumber() / 100;
+}
+
+export const useLenientThresholdPercentage = createNamedHook('useLenientThresholdPercentage', useLenientThresholdPercentageImpl);
