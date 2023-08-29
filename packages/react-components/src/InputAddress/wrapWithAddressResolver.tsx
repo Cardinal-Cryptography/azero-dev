581db32c9758396c5e379c5ec3af079cd2662db6
Wojciech Basiura <basiura.wojciech@gmail.com>
2023-08-29 10:33:59 +0200

    A0-2985: Refactor HOC

--- packages/react-components/src/InputAddress/wrapWithAddressResolver.tsx
+++ packages/react-components/src/InputAddress/wrapWithAddressResolver.tsx
@@ -3,64 +3,59 @@ import React, { ComponentType, useContext, useEffect, useState } from 'react';
 
 import { ApiCtx } from '@polkadot/react-api';
 import { systemNameToChainId } from '@polkadot/react-hooks';
+import { KeyringOptions, KeyringSectionOptions } from '@polkadot/ui-keyring/options/types';
 
-type ExpectedProps = {
-  options: {value: string | undefined}[];
-  optionsAll: Record<string, {value: string | undefined}[]>;
+type RequiredProps = {
+  options?: KeyringSectionOptions | null;
+  optionsAll?: KeyringOptions;
 };
 
-type RequiredProps = ExpectedProps & {
-  addressToDomain: Record<string, string | null | undefined>;
-};
+const wrapWithAddressResolver = <Props extends RequiredProps>(Component: ComponentType<Props>): ComponentType<Omit<Props, 'addressToDomain'>> => {
+  const Wrapped = (props: Props) => {
+    const [addressToDomain, setAddressToDomain] = useState<Record<string, string | null | undefined>>({});
+    const { api, systemChain } = useContext(ApiCtx);
 
-type ResolvedAddressesWrapperFactoryType = <
-  WrappedComponentProps extends RequiredProps
->(Component: ComponentType<WrappedComponentProps>, props: WrappedComponentProps) => React.ReactElement | null;
+    const { options, optionsAll } = props;
 
-type WrapWithAddressResolver = <
-  WrappedComponentProps extends RequiredProps
->(Component: ComponentType<WrappedComponentProps>) => (props: WrappedComponentProps) => React.ReactElement | null;
+    useEffect(() => {
+      const chainId = systemNameToChainId.get(systemChain);
 
-const ResolvedAddressesWrapperFactory: ResolvedAddressesWrapperFactoryType = (Component, props) => {
-  const [addressToDomain, setAddressToDomain] = useState({});
-  const { api, systemChain } = useContext(ApiCtx);
-
-  const { options, optionsAll } = props;
-
-  useEffect(() => {
-    const chainId = systemNameToChainId.get(systemChain);
-
-    if (!chainId) {
-      return;
-    }
-
-    const allAddressesWithDuplicates = [...(options || []), ...(optionsAll?.allPlus || [])].flatMap(({ value }) => value ? [value] : []);
-    const allAddresses = [...new Set(allAddressesWithDuplicates)];
-
-    const unresolvedAddresses = allAddresses.filter((address) => !(address in addressToDomain));
-    const domainPromises = unresolvedAddresses.map((address) => resolveAddressToDomain(address, { chainId, customApi: api }));
-
-    if (!domainPromises.length) {
-      return;
-    }
-
-    Promise.all(domainPromises).then(
-      (results) => {
-        const addressDomainTuples = results.flatMap(({ error, primaryDomain }, index) => error ? [] : [[unresolvedAddresses[index], primaryDomain] as [string, string | undefined | null]]);
-
-        setAddressToDomain({ ...addressToDomain, ...Object.fromEntries(addressDomainTuples) });
+      if (!chainId) {
+        return;
       }
-    ).catch(console.error);
-  }, [addressToDomain, api, options, optionsAll, systemChain]);
 
-  return (
-    <Component
-      {...props}
-      addressToDomain={addressToDomain}
-    />
-  );
+      const allAddressesWithDuplicates = [...(options || []), ...(optionsAll?.allPlus || [])].flatMap(({ value }) => value ? [value] : []);
+      const allAddresses = [...new Set(allAddressesWithDuplicates)];
+
+      const unresolvedAddresses = allAddresses.filter((address) => !(address in addressToDomain));
+      const domainPromises = unresolvedAddresses.map((address) => resolveAddressToDomain(address, { chainId, customApi: api }));
+
+      if (!domainPromises.length) {
+        return;
+      }
+
+      Promise.all(domainPromises).then(
+        (results) => {
+          const addressDomainTuples = results.flatMap(
+            ({ error, primaryDomain }, index) => error
+              ? []
+              : [[unresolvedAddresses[index], primaryDomain] as [string, string | undefined | null]]
+          );
+
+          setAddressToDomain({ ...addressToDomain, ...Object.fromEntries(addressDomainTuples) });
+        }
+      ).catch(console.error);
+    }, [addressToDomain, api, options, optionsAll, systemChain]);
+
+    return (
+      <Component
+        {...props}
+        addressToDomain={addressToDomain}
+      />
+    );
+  };
+
+  return Wrapped as ComponentType<Omit<Props, 'addressToDomain'>>;
 };
-
-const wrapWithAddressResolver: WrapWithAddressResolver = (Component) => (props) => ResolvedAddressesWrapperFactory(Component, props);
 
 export default wrapWithAddressResolver;
