fd1117270ef33a8a811f8e081be318d305356375
Wojciech Basiura <basiura.wojciech@gmail.com>
2023-09-01 17:31:04 +0200

    A0-2985: Add link to AZERO.ID domain in external links

--- packages/react-components/src/AzeroId/index.ts
+++ packages/react-components/src/AzeroId/index.ts
@@ -1,2 +1,2 @@
-export { AzeroIdDomain, AzeroIdInteractiveDomain, AzeroIdRegisterLink } from './Atoms.js';
+export { AzeroIdDomain, AzeroIdInteractiveDomain, AzeroIdRegisterLink, AZERO_ID_ROW_HEIGHT } from './Atoms.js';
 export { default } from './AzeroId.js';
