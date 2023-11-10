67fd64298d3bd685e6af0feb78275e8053d33407
Marcin <marcin.radecki@cardinals.cc>
2023-11-10 14:21:48 +0100

    Hardcoded number of max number of nominators per validator to 1

--- packages/page-staking/src/Actions/partials/Nominate.tsx
+++ packages/page-staking/src/Actions/partials/Nominate.tsx
@@ -58,11 +58,7 @@ function Nominate ({ className = '', controllerId, nominating, onChange, poolId,
     }
   }, [api, onChange, poolId, selected]);
 
-  const maxNominations = api.consts.staking.maxNominatorRewardedPerValidator
-    ? api.consts.staking.maxNominatorRewardedPerValidator.toNumber()
-    : api.consts.staking.maxNominations
-      ? (api.consts.staking.maxNominations as unknown as BN).toNumber()
-      : MAX_NOMINATIONS;
+  const maxNominations = MAX_NOMINATIONS;
 
   return (
     <StyledDiv className={className}>
