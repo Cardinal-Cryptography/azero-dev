82bab8af95fa552ff5d5e83f349b3e58db69e581
Wojciech Basiura <basiura.wojciech@gmail.com>
2024-10-10 12:10:57 +0200

    L1-63: Resolve apps-config

--- packages/apps-config/src/ui/logos/chains/index.ts
+++ packages/apps-config/src/ui/logos/chains/index.ts
@@ -7,122 +7,40 @@ export { chains3dpassSVG } from './generated/3dpassSVG.js';
 export { chainsAbandPNG } from './generated/abandPNG.js';
 export { chainsAcurastPNG } from './generated/acurastPNG.js';
 export { chainsAlephSVG } from './generated/alephSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsAltairSVG } from './generated/altairSVG.js';
-export { chainsAmplitudeSVG } from './generated/amplitudeSVG.js';
-export { chainsBitgreenPNG } from './generated/bitgreenPNG.js';
-=======
 export { chainsAllfeatSVG } from './generated/allfeatSVG.js';
-export { chainsAltairSVG } from './generated/altairSVG.js';
-export { chainsAmplitudeSVG } from './generated/amplitudeSVG.js';
 export { chainsAnalogSVG } from './generated/analogSVG.js';
 export { chainsArgonSVG } from './generated/argonSVG.js';
 export { chainsAssethubKusamaSVG } from './generated/assethub-kusamaSVG.js';
-export { chainsBitgreenPNG } from './generated/bitgreenPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsBittensorPNG } from './generated/bittensorPNG.js';
 export { chainsChainflipPNG } from './generated/chainflipPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsComposableFinancePNG } from './generated/composableFinancePNG.js';
-export { chainsCreditcoinTestPNG } from './generated/creditcoin-testPNG.js';
-export { chainsCreditcoinPNG } from './generated/creditcoinPNG.js';
-=======
 export { chainsCommuneaiPNG } from './generated/communeaiPNG.js';
-export { chainsComposableFinancePNG } from './generated/composableFinancePNG.js';
 export { chainsCoretimeKusamaSVG } from './generated/coretime-kusamaSVG.js';
-export { chainsCreditcoinTestPNG } from './generated/creditcoin-testPNG.js';
-export { chainsCreditcoinPNG } from './generated/creditcoinPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsDebioSVG } from './generated/debioSVG.js';
 export { chainsFerrumPNG } from './generated/ferrumPNG.js';
 export { chainsFragnovaPNG } from './generated/fragnovaPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsFrequencySVG } from './generated/frequencySVG.js';
-export { chainsGeminisPNG } from './generated/geminisPNG.js';
-export { chainsGenshiroSVG } from './generated/genshiroSVG.js';
-=======
 export { chainsFrequencyPaseoSVG } from './generated/frequencyPaseoSVG.js';
-export { chainsFrequencySVG } from './generated/frequencySVG.js';
-export { chainsGeminisPNG } from './generated/geminisPNG.js';
-export { chainsGenshiroSVG } from './generated/genshiroSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsGmJPEG } from './generated/gmJPEG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsHydratePNG } from './generated/hydratePNG.js';
-=======
 export { chainsHydrationPaseoSVG } from './generated/hydration-paseoSVG.js';
 export { chainsHydrationSVG } from './generated/hydrationSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsIdiyanaleLogoWhiteSVG } from './generated/idiyanale_logo_whiteSVG.js';
 export { chainsInvarchJPEG } from './generated/invarchJPEG.js';
 export { chainsJurPNG } from './generated/jurPNG.js';
 export { chainsKreivoSVG } from './generated/kreivoSVG.js';
 export { chainsKusamaSVG } from './generated/kusamaSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsListenPNG } from './generated/listenPNG.js';
-export { chainsLogionPNG } from './generated/logionPNG.js';
-export { chainsMangataPNG } from './generated/mangataPNG.js';
-=======
 export { chainsLaosSigmaPNG } from './generated/laos-sigmaPNG.js';
 export { chainsLaosPNG } from './generated/laosPNG.js';
 export { chainsLiberlandPNG } from './generated/liberlandPNG.js';
-export { chainsListenPNG } from './generated/listenPNG.js';
-export { chainsLogionPNG } from './generated/logionPNG.js';
-export { chainsMangataPNG } from './generated/mangataPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsMangataxPNG } from './generated/mangataxPNG.js';
 export { chainsMyriadPNG } from './generated/myriadPNG.js';
 export { chainsMyxcavPNG } from './generated/myxcavPNG.js';
 export { chainsNeurowebTestnetPNG } from './generated/neuroweb-testnetPNG.js';
 export { chainsNeurowebPNG } from './generated/neurowebPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsOakPNG } from './generated/oakPNG.js';
-=======
-export { chainsOakPNG } from './generated/oakPNG.js';
 export { chainsPaseoPNG } from './generated/paseoPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsPeaqPNG } from './generated/peaqPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsPendulumSVG } from './generated/pendulumSVG.js';
-export { chainsPicassoSVG } from './generated/picassoSVG.js';
-export { chainsPolkadotCircleSVG } from './generated/polkadot-circleSVG.js';
-=======
-export { chainsPendulumSVG } from './generated/pendulumSVG.js';
 export { chainsPeopleKusamaSVG } from './generated/people-kusamaSVG.js';
 export { chainsPeoplePolkadotSVG } from './generated/people-polkadotSVG.js';
-export { chainsPicassoSVG } from './generated/picassoSVG.js';
-export { chainsPolkadotCircleSVG } from './generated/polkadot-circleSVG.js';
 export { chainsPopNetworkSVG } from './generated/pop-networkSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsQpnPNG } from './generated/qpnPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { chainsRiodefiPNG } from './generated/riodefiPNG.js';
-export { chainsRococoSVG } from './generated/rococoSVG.js';
-export { chainsShidenPNG } from './generated/shidenPNG.js';
-export { chainsSkyekiwiPNG } from './generated/skyekiwiPNG.js';
-export { chainsSnakenetSVG } from './generated/snakenetSVG.js';
-export { chainsSnowbridgePNG } from './generated/snowbridgePNG.js';
-export { chainsSpannerPNG } from './generated/spannerPNG.js';
-export { chainsStandardPNG } from './generated/standardPNG.js';
-export { chainsT0rnPNG } from './generated/t0rnPNG.js';
-=======
-export { chainsRiodefiPNG } from './generated/riodefiPNG.js';
-export { chainsRococoSVG } from './generated/rococoSVG.js';
-export { chainsShidenPNG } from './generated/shidenPNG.js';
-export { chainsSkyekiwiPNG } from './generated/skyekiwiPNG.js';
-export { chainsSnowbridgePNG } from './generated/snowbridgePNG.js';
-export { chainsSpannerPNG } from './generated/spannerPNG.js';
-export { chainsStandardPNG } from './generated/standardPNG.js';
-export { chainsT0rnPNG } from './generated/t0rnPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { chainsTanglePNG } from './generated/tanglePNG.js';
 export { chainsVaraTestnetPNG } from './generated/vara-testnetPNG.js';
 export { chainsVaraPNG } from './generated/varaPNG.js';
--- packages/apps-config/src/ui/logos/nodes/index.ts
+++ packages/apps-config/src/ui/logos/nodes/index.ts
@@ -4,264 +4,44 @@
 // Do not edit. Auto-generated via node scripts/imgConvert.mjs
 
 export { nodesAssetHubSVG } from './generated/assetHubSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesAstarPNG } from './generated/astarPNG.js';
-export { nodesAutomataPNG } from './generated/automataPNG.js';
-export { nodesAventusSVG } from './generated/aventusSVG.js';
-export { nodesBajunPNG } from './generated/bajunPNG.js';
-export { nodesBasiliskRococoBgPNG } from './generated/basilisk-rococo-bgPNG.js';
-export { nodesBasiliskPNG } from './generated/basiliskPNG.js';
-export { nodesBeastSVG } from './generated/beastSVG.js';
-export { nodesBifrostSVG } from './generated/bifrostSVG.js';
-export { nodesBitcountryPNG } from './generated/bitcountryPNG.js';
-=======
-export { nodesAstarPNG } from './generated/astarPNG.js';
-export { nodesAventusSVG } from './generated/aventusSVG.js';
-export { nodesBajunPNG } from './generated/bajunPNG.js';
-export { nodesBasiliskRococoBgPNG } from './generated/basilisk-rococo-bgPNG.js';
-export { nodesBasiliskPNG } from './generated/basiliskPNG.js';
-export { nodesBeastSVG } from './generated/beastSVG.js';
-export { nodesBifrostSVG } from './generated/bifrostSVG.js';
-export { nodesBitcountryPNG } from './generated/bitcountryPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesBridgeHubBlackSVG } from './generated/bridgeHubBlackSVG.js';
 export { nodesBridgeHubSVG } from './generated/bridgeHubSVG.js';
 export { nodesCereSVG } from './generated/cereSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesCessPNG } from './generated/cessPNG.js';
-export { nodesChainxSVG } from './generated/chainxSVG.js';
-export { nodesCloverSVG } from './generated/cloverSVG.js';
-export { nodesCoinversationPNG } from './generated/coinversationPNG.js';
-export { nodesCompetitorsClubPNG } from './generated/competitors-clubPNG.js';
-export { nodesConftiSVG } from './generated/conftiSVG.js';
-export { nodesCrabSVG } from './generated/crabSVG.js';
-export { nodesCrownSterlingPNG } from './generated/crown-sterlingPNG.js';
-export { nodesCrustMaxwellSVG } from './generated/crust-maxwellSVG.js';
-export { nodesCrustParachainSVG } from './generated/crustParachainSVG.js';
-export { nodesCrustSVG } from './generated/crustSVG.js';
-=======
-export { nodesCessPNG } from './generated/cessPNG.js';
-export { nodesChainxSVG } from './generated/chainxSVG.js';
-export { nodesCloverSVG } from './generated/cloverSVG.js';
-export { nodesCoinversationPNG } from './generated/coinversationPNG.js';
-export { nodesCompetitorsClubPNG } from './generated/competitors-clubPNG.js';
-export { nodesConftiSVG } from './generated/conftiSVG.js';
 export { nodesContinuumPNG } from './generated/continuumPNG.js';
-export { nodesCrabSVG } from './generated/crabSVG.js';
-export { nodesCrownSterlingPNG } from './generated/crown-sterlingPNG.js';
-export { nodesCrustMaxwellSVG } from './generated/crust-maxwellSVG.js';
-export { nodesCrustParachainSVG } from './generated/crustParachainSVG.js';
-export { nodesCrustSVG } from './generated/crustSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesCurioSVG } from './generated/curioSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesDaliPNG } from './generated/daliPNG.js';
-export { nodesDarwiniaSVG } from './generated/darwiniaSVG.js';
-export { nodesDatahighwayPNG } from './generated/datahighwayPNG.js';
-export { nodesDockPNG } from './generated/dockPNG.js';
-export { nodesDolphinSVG } from './generated/dolphinSVG.js';
-export { nodesDotmogSVG } from './generated/dotmogSVG.js';
-export { nodesEaveSVG } from './generated/eaveSVG.js';
-export { nodesEdgewareWhitePNG } from './generated/edgeware-whitePNG.js';
-export { nodesEfinitySVG } from './generated/efinitySVG.js';
-export { nodesEncointerBlueSVG } from './generated/encointer-blueSVG.js';
-=======
-export { nodesDaliPNG } from './generated/daliPNG.js';
 export { nodesDanceboxSVG } from './generated/danceboxSVG.js';
 export { nodesDarwiniaKoiSVG } from './generated/darwinia-koiSVG.js';
-export { nodesDarwiniaSVG } from './generated/darwiniaSVG.js';
-export { nodesDatahighwayPNG } from './generated/datahighwayPNG.js';
-export { nodesDockPNG } from './generated/dockPNG.js';
-export { nodesDolphinSVG } from './generated/dolphinSVG.js';
-export { nodesDotmogSVG } from './generated/dotmogSVG.js';
-export { nodesEaveSVG } from './generated/eaveSVG.js';
-export { nodesEdgewareWhitePNG } from './generated/edgeware-whitePNG.js';
-export { nodesEfinitySVG } from './generated/efinitySVG.js';
 export { nodesElysiumPNG } from './generated/elysiumPNG.js';
-export { nodesEncointerBlueSVG } from './generated/encointer-blueSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesEwxSVG } from './generated/ewxSVG.js';
 export { nodesGiantPNG } from './generated/giantPNG.js';
 export { nodesHanyonycashPNG } from './generated/hanyonycashPNG.js';
 export { nodesHumanodePNG } from './generated/humanodePNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesIdavollPNG } from './generated/idavollPNG.js';
-export { nodesImbuePNG } from './generated/imbuePNG.js';
-=======
 export { nodesHyperbridgePNG } from './generated/hyperbridgePNG.js';
-export { nodesIdavollPNG } from './generated/idavollPNG.js';
 export { nodesIdealNetworkSVG } from './generated/ideal-networkSVG.js';
-export { nodesImbuePNG } from './generated/imbuePNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesInnovatorPNG } from './generated/innovatorPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesIntegriteeSVG } from './generated/integriteeSVG.js';
-export { nodesInterlaySVG } from './generated/interlaySVG.js';
-export { nodesIpciSVG } from './generated/ipciSVG.js';
-export { nodesIpsePNG } from './generated/ipsePNG.js';
-export { nodesIpseSVG } from './generated/ipseSVG.js';
-export { nodesJazPNG } from './generated/jazPNG.js';
-export { nodesJoystreamSVG } from './generated/joystreamSVG.js';
-export { nodesJupiterSVG } from './generated/jupiterSVG.js';
-export { nodesKabochaSVG } from './generated/kabochaSVG.js';
-=======
-export { nodesIntegriteeSVG } from './generated/integriteeSVG.js';
-export { nodesInterlaySVG } from './generated/interlaySVG.js';
 export { nodesInvoPNG } from './generated/invoPNG.js';
-export { nodesIpciSVG } from './generated/ipciSVG.js';
-export { nodesIpsePNG } from './generated/ipsePNG.js';
-export { nodesIpseSVG } from './generated/ipseSVG.js';
-export { nodesJazPNG } from './generated/jazPNG.js';
-export { nodesJoystreamSVG } from './generated/joystreamSVG.js';
-export { nodesJupiterSVG } from './generated/jupiterSVG.js';
-export { nodesKabochaSVG } from './generated/kabochaSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesKarmachainSVG } from './generated/karmachainSVG.js';
-<<<<<<< HEAD
-export { nodesKlaosPNG } from './generated/klaosPNG.js';
-||||||| 2b40308a49
-export { nodesKhalaSVG } from './generated/khalaSVG.js';
-export { nodesKiltPNG } from './generated/kiltPNG.js';
-export { nodesKlaosPNG } from './generated/klaosPNG.js';
-export { nodesKlugPNG } from './generated/klugPNG.js';
-export { nodesKonomiPNG } from './generated/konomiPNG.js';
-=======
-export { nodesKhalaSVG } from './generated/khalaSVG.js';
-export { nodesKiltPNG } from './generated/kiltPNG.js';
 export { nodesKineraPNG } from './generated/kineraPNG.js';
-export { nodesKlugPNG } from './generated/klugPNG.js';
-export { nodesKonomiPNG } from './generated/konomiPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesKrestPNG } from './generated/krestPNG.js';
 export { nodesLitentryRococoPNG } from './generated/litentry-rococoPNG.js';
-<<<<<<< HEAD
-export { nodesMetaquityNetworkPNG } from './generated/metaquity-networkPNG.js';
-||||||| 2b40308a49
-export { nodesLitentryPNG } from './generated/litentryPNG.js';
-export { nodesLitmusPNG } from './generated/litmusPNG.js';
-export { nodesLoomNetworkPNG } from './generated/loom_networkPNG.js';
-export { nodesMantaPNG } from './generated/mantaPNG.js';
-export { nodesMathSVG } from './generated/mathSVG.js';
-export { nodesMd5PNG } from './generated/md5PNG.js';
-export { nodesMetaquityNetworkPNG } from './generated/metaquity-networkPNG.js';
-export { nodesMinixPNG } from './generated/minixPNG.js';
-=======
-export { nodesLitentryPNG } from './generated/litentryPNG.js';
-export { nodesLitmusPNG } from './generated/litmusPNG.js';
-export { nodesLoomNetworkPNG } from './generated/loom_networkPNG.js';
 export { nodesMagnetPNG } from './generated/magnetPNG.js';
 export { nodesMandalaPNG } from './generated/mandalaPNG.js';
-export { nodesMantaPNG } from './generated/mantaPNG.js';
-export { nodesMathSVG } from './generated/mathSVG.js';
-export { nodesMd5PNG } from './generated/md5PNG.js';
-export { nodesMinixPNG } from './generated/minixPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesMoonbaseAlphaSVG } from './generated/moonbase_alphaSVG.js';
 export { nodesMoonbeamSVG } from './generated/moonbeamSVG.js';
 export { nodesMoonsamaSVG } from './generated/moonsamaSVG.js';
 export { nodesMusePNG } from './generated/musePNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesMybankPNG } from './generated/mybankPNG.js';
-export { nodesNftmartPNG } from './generated/nftmartPNG.js';
-export { nodesNodleSVG } from './generated/nodleSVG.js';
-export { nodesOliSVG } from './generated/oliSVG.js';
-export { nodesOmnibtcSVG } from './generated/omnibtcSVG.js';
-export { nodesOpalLogoPNG } from './generated/opal-logoPNG.js';
-export { nodesOpportunityPNG } from './generated/opportunityPNG.js';
-=======
-export { nodesMybankPNG } from './generated/mybankPNG.js';
 export { nodesMyriadPaseoSVG } from './generated/myriadPaseoSVG.js';
 export { nodesMythosPNG } from './generated/mythosPNG.js';
-export { nodesNftmartPNG } from './generated/nftmartPNG.js';
-export { nodesNodleSVG } from './generated/nodleSVG.js';
-export { nodesOliSVG } from './generated/oliSVG.js';
-export { nodesOmnibtcSVG } from './generated/omnibtcSVG.js';
-export { nodesOpalLogoPNG } from './generated/opal-logoPNG.js';
-export { nodesOpportunityPNG } from './generated/opportunityPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesOzPNG } from './generated/ozPNG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesPangolinSVG } from './generated/pangolinSVG.js';
-export { nodesPangoroSVG } from './generated/pangoroSVG.js';
-export { nodesParallelSVG } from './generated/parallelSVG.js';
-=======
-export { nodesPangolinSVG } from './generated/pangolinSVG.js';
-export { nodesParallelSVG } from './generated/parallelSVG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesParamiJPEG } from './generated/paramiJPEG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesPhalaSVG } from './generated/phalaSVG.js';
-export { nodesPhoenixPNG } from './generated/phoenixPNG.js';
-export { nodesPicassoPNG } from './generated/picassoPNG.js';
-export { nodesPichiuPNG } from './generated/pichiuPNG.js';
-export { nodesPlasmPNG } from './generated/plasmPNG.js';
-=======
 export { nodesPeerplaysPNG } from './generated/peerplaysPNG.js';
-export { nodesPhalaSVG } from './generated/phalaSVG.js';
-export { nodesPhoenixPNG } from './generated/phoenixPNG.js';
 export { nodesPhykenNetworkPNG } from './generated/phyken-networkPNG.js';
-export { nodesPicassoPNG } from './generated/picassoPNG.js';
-export { nodesPichiuPNG } from './generated/pichiuPNG.js';
-export { nodesPlasmPNG } from './generated/plasmPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesPolimecSVG } from './generated/polimecSVG.js';
-<<<<<<< HEAD
-||||||| 2b40308a49
-export { nodesPolkadexSVG } from './generated/polkadexSVG.js';
-export { nodesPolkafoundrySVG } from './generated/polkafoundrySVG.js';
-export { nodesPolkasmithSVG } from './generated/polkasmithSVG.js';
-export { nodesPolymeshSVG } from './generated/polymeshSVG.js';
-export { nodesPontemSVG } from './generated/pontemSVG.js';
-export { nodesPrismPNG } from './generated/prismPNG.js';
-export { nodesQuartzPNG } from './generated/quartzPNG.js';
-export { nodesRealisPNG } from './generated/realisPNG.js';
-=======
-export { nodesPolkadexSVG } from './generated/polkadexSVG.js';
-export { nodesPolkafoundrySVG } from './generated/polkafoundrySVG.js';
-export { nodesPolkasmithSVG } from './generated/polkasmithSVG.js';
-export { nodesPolymeshSVG } from './generated/polymeshSVG.js';
-export { nodesPontemSVG } from './generated/pontemSVG.js';
-export { nodesPrismPNG } from './generated/prismPNG.js';
-export { nodesQuartzPNG } from './generated/quartzPNG.js';
-export { nodesRealisPNG } from './generated/realisPNG.js';
 export { nodesRegionxCocosPNG } from './generated/regionxCocosPNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesRexSVG } from './generated/rexSVG.js';
 export { nodesSocietalSVG } from './generated/societalSVG.js';
 export { nodesT1rnPNG } from './generated/t1rnPNG.js';
 export { nodesT3rnPNG } from './generated/t3rnPNG.js';
 export { nodesThebifrostPNG } from './generated/thebifrostPNG.js';
-<<<<<<< HEAD
-export { nodesTrappistRococoPNG } from './generated/trappist-rococoPNG.js';
-||||||| 2b40308a49
-export { nodesTrappistRococoPNG } from './generated/trappist-rococoPNG.js';
-export { nodesTrustbasePNG } from './generated/trustbasePNG.js';
-export { nodesUniartsPNG } from './generated/uniartsPNG.js';
-export { nodesUniqueSVG } from './generated/uniqueSVG.js';
-export { nodesUnitnetworkPNG } from './generated/unitnetworkPNG.js';
-export { nodesUnitvPNG } from './generated/unitvPNG.js';
-export { nodesValiuPNG } from './generated/valiuPNG.js';
-export { nodesVaraSVG } from './generated/varaSVG.js';
-export { nodesWeb3gamesSVG } from './generated/web3gamesSVG.js';
-export { nodesWestendColourSVG } from './generated/westend_colourSVG.js';
-=======
-export { nodesTrustbasePNG } from './generated/trustbasePNG.js';
-export { nodesUniartsPNG } from './generated/uniartsPNG.js';
-export { nodesUniqueSVG } from './generated/uniqueSVG.js';
-export { nodesUnitnetworkPNG } from './generated/unitnetworkPNG.js';
-export { nodesUnitvPNG } from './generated/unitvPNG.js';
-export { nodesValiuPNG } from './generated/valiuPNG.js';
-export { nodesVaraSVG } from './generated/varaSVG.js';
-export { nodesWeb3gamesSVG } from './generated/web3gamesSVG.js';
-export { nodesWestendColourSVG } from './generated/westend_colourSVG.js';
 export { nodesXodePNG } from './generated/xodePNG.js';
->>>>>>> a0-ops-upstream-automerge
 export { nodesYerbanetworkPNG } from './generated/yerbanetworkPNG.js';
