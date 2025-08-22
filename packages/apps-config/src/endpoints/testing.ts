98daca65400be8e907e1d076593bdc0c60412cab
Milos Kriz <82968568+miloskriz@users.noreply.github.com>
2025-08-22 09:23:33 +0100

    Add RPC support from Community
    This commit adds the endpoints from the Community Infrastructure initiative

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -16,7 +16,8 @@ export const testChains: Omit<EndpointOption, 'teleport'>[] = [{
   info: 'aleph-testnet',
   isPeopleForIdentity: false,
   providers: {
-    'Aleph Zero Foundation': 'wss://ws.test.azero.dev'
+    'Aleph Zero Foundation': 'wss://ws.test.azero.dev',
+    'Community Infrastructure': 'wss://testnet.azero.fans'
   },
   text: 'Aleph Zero Testnet',
   ui: {
